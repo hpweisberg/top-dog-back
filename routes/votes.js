28ceb09fb30a62eb82edb31b0cc40b14a07140fe
hpweisberg <hpweisberg@gmail.com>
2023-02-27 13:27:40 -0800

    update route for put and castVote function to be a put request that accounts for previus votes

--- routes/votes.js
+++ routes/votes.js
@@ -9,6 +9,7 @@ const { decodeUserFromToken, checkAuth } = middleware
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
+router.put('/', checkAuth, votesCtrl.castVote)
 
 
 module.exports = router
